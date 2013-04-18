72618ec56c7d261618fe1d03fc171d7ec307581f
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-04-18 09:37:19 -0700

    UIActivitiyIndicatatorView: instantiate already started

---
+++ Categories/UIActivityIndicatorView+Start.h
@@ -0,0 +1,16 @@
+//
+//  UIActivityIndicatorView+Start.h
+//  Yerdle
+//
+//  Created by Hugues Bernet-Rollande on 4/17/13.
+//  Copyright (c) 2013 Hugues Bernet-Rollande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIActivityIndicatorView (Start)
+
++ (id)activityIndicatorStarted;
++ (id)activityIndicatorStartedWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style;
+
+@end
---
+++ Categories/UIActivityIndicatorView+Start.m
@@ -0,0 +1,25 @@
+//
+//  UIActivityIndicatorView+Start.m
+//  Yerdle
+//
+//  Created by Hugues Bernet-Rollande on 4/17/13.
+//  Copyright (c) 2013 Hugues Bernet-Rollande. All rights reserved.
+//
+
+#import "UIActivityIndicatorView+Start.h"
+
+@implementation UIActivityIndicatorView (Start)
+
++ (id)activityIndicatorStarted;
+{
+    return [self activityIndicatorStartedWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+}
+
++ (id)activityIndicatorStartedWithActivityIndicatorStyle:(UIActivityIndicatorViewStyle)style;
+{
+    UIActivityIndicatorView *activity = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:style];
+    [activity startAnimating];
+    return activity;
+}
+
+@end
