31312ffa86a3c4e68b349ed546926f003b847c70
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-06-14 14:32:07 -0700

    UIApplication+TopController

---
+++ Categories/UIApplication+TopController.h
@@ -0,0 +1,15 @@
+//
+//  UIApplication+TopController.h
+//  Yerdle XP 01
+//
+//  Created by Hugues Bernet-Rollande on 4/24/13.
+//  Copyright (c) 2013 Hugues Bernet-Rollande. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (TopController)
+
+- (UIViewController*) topController;
+
+@end
