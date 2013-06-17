2e816d31eb6da5e1206c450c4514c3ed59830f1c
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-06-17 15:38:30 -0700

    UIDevice+Version

---
+++ Categories/UIDevice+Version.h
@@ -0,0 +1,13 @@
+//
+//  UIDevice+Version.h
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 6/17/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (Version)
+
+@end
---
+++ Categories/UIDevice+Version.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+Version.m
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 6/17/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import "UIDevice+Version.h"
+
+@implementation UIDevice (Version)
+
+@end
