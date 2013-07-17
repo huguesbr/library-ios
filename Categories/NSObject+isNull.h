6a44bbe16d3e34e1d2549fab77d07872b0978421
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-07-17 16:59:00 -0700

    NSObject+isNull

---
+++ Categories/NSObject+isNull.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+isNull.h
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 7/17/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (isNull)
+- (BOOL)isNull;
+@end
---
+++ Categories/NSObject+isNull.m
@@ -0,0 +1,16 @@
+//
+//  NSObject+isNull.m
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 7/17/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import "NSObject+isNull.h"
+
+@implementation NSObject (isNull)
+- (BOOL)isNull;
+{
+    return [self isEqual:[NSNull null]];
+}
+@end
