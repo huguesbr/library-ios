4f02b04333f8422db797df19a1d45108e91a0422
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-07-23 18:28:23 -0700

    NSString+Crypto

---
+++ Categories/NSString+Crypto.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Crypto.h
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 7/23/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Crypto)
+
+- (NSString *)sha1;
+- (NSString *)md5;
+
+@end
---
+++ Categories/NSString+Crypto.m
@@ -0,0 +1,45 @@
+//
+//  NSString+Crypto.m
+//  yerdle2
+//
+//  Created by Hugues Bernet-Rollande on 7/23/13.
+//  Copyright (c) 2013 Yerdle. All rights reserved.
+//
+
+#import "NSString+Crypto.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation NSString (Crypto)
+
+- (NSString *)sha1;
+{
+    const char *cstr = [self cStringUsingEncoding:NSUTF8StringEncoding];
+    NSData *data = [NSData dataWithBytes:cstr length:self.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return output;
+}
+
+- (NSString *)md5;
+{
+    const char *cStr = [self UTF8String];
+    unsigned char digest[16];
+    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return  output;
+}
+
+@end
