dfd191affe06916fc10ea6e91989a9af3bb739be
Hugues Bernet-Rollande <hugues@xdev.fr>
2013-09-04 14:53:03 -0700

    YDPushNotificationHelper

--- Helpers/YDPushNotificationHelper.m
+++ Helpers/YDPushNotificationHelper.m
@@ -87,7 +87,7 @@
 
 + (void)prompt;
 {
-    [self promptWithMessage:NSLocalizedString(@"Would you like to receive notifications from yerdle about your items?", @"Notification Helper Prompt Message")];
+    [self promptWithMessage:NSLocalizedString(@"Would you like us to notify you about activity on your items?", @"Notification Helper Prompt Message")];
 }
 
 + (void)promptWithMessage:(NSString *)message;
