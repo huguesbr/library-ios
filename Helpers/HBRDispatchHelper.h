7df635907af3957e7685279e68f2871643f7fd1f
huguesbr <hugues@xdev.fr>
2013-11-20 15:44:06 -0800

    Adding some dispatch helper methods

--- Helpers/HBRDispatchHelper.h
+++ Helpers/HBRDispatchHelper.h
@@ -9,6 +9,8 @@
 //#ifndef yerdle2_HBRDispatchHelper_h
 //#define yerdle2_HBRDispatchHelper_h
 
-#define dispatch_async_main(call) dispatch_async(dispatch_get_main_queue(), ^{call})
+#define dispatch_async_background(...) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{ __VA_ARGS__ })
+#define dispatch_async_default(...) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{ __VA_ARGS__ })
+#define dispatch_async_main(...) dispatch_async(dispatch_get_main_queue(), ^{ __VA_ARGS__ })
 
 //#endif
