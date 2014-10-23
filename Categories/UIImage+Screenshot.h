88b2562b6d648c177eec31cf0aeba9f492c6ec40
huguesbr <hugues@xdev.fr>
2014-10-23 09:10:05 -0700

    Adding screenshot categories

---
+++ Categories/UIImage+Screenshot.h
@@ -0,0 +1,28 @@
+//
+//  UIImage+Screenshot.h
+//  yerdle
+//
+//  Created by Hugues Bernet-Rollande on 10/23/14.
+//  Copyright (c) 2014 Yerdle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@implementation UIImage (Screenshot)
+
++ (instancetype)screenshot
+{
+    UIWindow *window = [UIApplication sharedApplication].keyWindow;
+    CGSize windowSize = [window bounds].size;
+    
+    UIGraphicsBeginImageContext(windowSize);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [window.layer renderInContext:context];
+    
+    UIImage *screenshot = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return screenshot;
+}
+
+@end
---
+++ Categories/UIImage+Screenshot.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Screenshot.m
+//  yerdle
+//
+//  Created by Hugues Bernet-Rollande on 10/23/14.
+//  Copyright (c) 2014 Yerdle. All rights reserved.
+//
+
+#import "UIImage+Screenshot.h"
+
+@implementation UIImage (Screenshot)
++ (instancetype)screenshot;
+@end
